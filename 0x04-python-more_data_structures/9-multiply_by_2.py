33dba9d7150a2d1fadbb23d51d37ca7aefd4b46e
iamwairimu <kmnleah@gamil.com>
2024-01-10 14:10:02 -0800

    files done

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/pytthon3
+def multiply_by_2(a_dictionary):
+    new_dir = a_dictionary.copy()
+    list_key = list(new_dir.keys())
+    for i in list_key:
+        new_dir[i] *= 2
+    return (new_dir)
