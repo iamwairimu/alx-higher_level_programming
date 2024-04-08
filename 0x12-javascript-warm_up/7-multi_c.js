02bf67cdfa8a7b7890340a1b620d38184ff5f4c1
iamwairimu <kmnleah@gmail.com>
2024-04-08 15:16:16 -0700

    print as many times as possible

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const x = process.argv[2];
+let j = 0;
+if (!parseInt(x)) {
+  console.log('Missing number of occurrences');
+} else {
+  while (j < x) {
+	  console.log('C is fun');
+	  j++;
+  }
+}
