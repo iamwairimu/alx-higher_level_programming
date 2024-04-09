3aaabdf70d4af88e40d2829cdd19363b313781ab
iamwairimu <kmnleah@gmail.com>
2024-04-09 14:18:32 -0700

    print x char

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+/**
+ * print using x character
+ */
+class Rectangle {
+  constructor (w, h) {
+    if (typeof w === 'number' && w > 0 && typeof h === 'number' && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let y = 0;
+      let myVar = '';
+      while (y < this.width) {
+        myVar += 'X';
+        y++;
+      }
+      console.log(myVar);
+    }
+  }
+}
+module.exports = Rectangle;
