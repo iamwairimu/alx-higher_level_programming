be66198ab885e254013bfae422468a8004a5527d
iamwairimu <kmnleah@gmail.com>
2024-04-09 14:01:44 -0700

    rec 2 args

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+/**
+ * Define rectangle class and width and height
+ */
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+module.exports = Rectangle;
