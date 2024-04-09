feb1dd791e4cb6348df5b83be7f8eabc06da2869
iamwairimu <kmnleah@gmail.com>
2024-04-09 14:07:27 -0700

    error message

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+/**
+ * 2 args and if negative print error
+ */
+class Rectangle {
+  constructor (w, h) {
+    if (typeof w === 'number' && w > 0 && typeof h === 'number' && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+}
+module.exports = Rectangle;
